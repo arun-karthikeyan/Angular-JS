534ab5812934e100d5c519fef4cfd4ddf367b505
mecodesta <the3drevolution@gmail.com>
2016-03-06 23:37:27 -0700

    Angular Forms and Form Validation

--- Week 2/Gulp/conFusion/app/scripts/app.js
+++ Week 2/Gulp/conFusion/app/scripts/app.js
@@ -48,6 +48,7 @@ app.controller('MenuController',['$scope', function($scope){
   //Doing this makes the 'dishes' object available to the HTML
   $scope.dishes = dishes;
 
+
   $scope.select = function(setTab){
     $scope.tab = setTab;
     if(setTab === 2){
@@ -69,4 +70,50 @@ app.controller('MenuController',['$scope', function($scope){
     $scope.showDetails = !$scope.showDetails;
   };
 
+}])
+//Parent Controller
+.controller('ContactController', ['$scope', function($scope){
+
+$scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"", invalidChannelSelection: false, comments:"", tel:{number:"", areaCode:""}};
+var channels = [{
+  value:"tel", label:"Tel."
+},
+{
+  value:"email", label:"Email"
+}];
+
+$scope.channels = channels;
+}])
+//Child Controller - inherits the scope of the Parent Controller (ContactController)
+.controller('FeedbackController', ['$scope', function($scope){
+$scope.sendFeedback = function() {
+  console.log($scope.feedback);
+
+  //JS side validation of 'feedbackForm'
+  if($scope.feedback.agree && (($scope.feedback.mychannel==="") || $scope.feedback.mychannel===null || $scope.feedback.mychannel===undefined)){
+    $scope.feedback.invalidChannelSelection = true;
+    console.log('incorrect');
+  }else{
+
+    //doesn't work because the below statement is treated as a primitive and a new child primitive is created
+    // $scope.feedback = {
+    //   mychannel:"", firstName:"", lastName:"", agree:false, email:""
+    // };
+
+    //the below correctly updates the parent scope objects
+    $scope.feedback.mychannel="";
+    $scope.feedback.firstName="";
+    $scope.feedback.lastName="";
+    $scope.feedback.agree=false;
+    $scope.feedback.email="";
+    $scope.feedback.invalidChannelSelection=false;
+    $scope.feedback.comments="";
+    $scope.feedback.tel.number="";
+    $scope.feedback.tel.areaCode="";
+    $scope.feedbackForm.$setPristine();
+    console.log('correct');
+    //logging the re-initialized empty feedback
+    console.log($scope.feedback);
+  }
+};
 }]);
--- Week 2/Gulp/conFusion/gulpfile.js
+++ Week 2/Gulp/conFusion/gulpfile.js
@@ -1,92 +1,128 @@
+//I'm a strict officer :P
+'use strict';
+
+//loading all the dev plugins into gulp
 var gulp = require('gulp'),
-minifycss = require('gulp-minify-css'),
 jshint = require('gulp-jshint'),
 stylish = require('jshint-stylish'),
+cleancss = require('gulp-clean-css'),
 uglify = require('gulp-uglify'),
 usemin = require('gulp-usemin'),
+htmlmin = require('gulp-htmlmin'),
 imagemin = require('gulp-imagemin'),
 rename = require('gulp-rename'),
-concat = require('gulp-concat'),
 notify = require('gulp-notify'),
 cache = require('gulp-cache'),
-changed = require('gulp-changed'),
 rev = require('gulp-rev'),
+changed = require('gulp-changed'),
 browserSync = require('browser-sync').create(),
+del = require('del'),
 reload = browserSync.reload,
-ngannotate = require('gulp-ng-annotate'),
-del = require('del');
+gulpngannonate = require('gulp-ng-annotate'),
+merge = require('merge-stream');
 
-//JsHint Gulp Task
+var runsequence = require('run-sequence').use(gulp);
+
+//Gulp JS Hint task - JS Hint must run synchronously before clean, so if there is any error it has to stop right there
 gulp.task('jshint', function(){
   return gulp.src('app/scripts/**/*.js')
+  .pipe(changed('dist/'))
   .pipe(jshint('.jshintrc'))
   .pipe(jshint.reporter(stylish));
 });
 
-//Clean Task
-gulp.task('clean', function(){
-  //removes the 'dist' folder completely
-  return del(['dist']);
-});
-
-//usemin task
-gulp.task('usemin', ['jshint'], function(){
-  return gulp.src('app/menu.html')
-  .pipe(usemin({
-    css: [minifycss(), rev()],
-    js: [ngannotate(), uglify(), rev()]
-  }))
-  .pipe(gulp.dest('dist/'));
-  // .pipe(browserSync.stream());
-});
-
-//imagemin task
+//Gulp Imagemin task
 gulp.task('imagemin', function(){
-  return del(['dist/images']), gulp.src('app/images/**/*')
+  return gulp.src('app/images/**/*')
   .pipe(cache(imagemin({
     optimizationLevel: 3,
     progressive: true,
     interlaced: true
   })))
   .pipe(gulp.dest('dist/images'))
-  // .pipe(browserSync.stream())
   .pipe(notify({
-    onLast: true,
-    message: 'Images task complete'
+    message: 'All Images Optimized',
+    onLast: true
   }));
 });
 
-//copyFonts Task
-gulp.task('copyfonts', function(){
-  //No transformation required for these files, they're just being copied to the distribution folder
-  return del(['dist/fonts']), gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*')
-  .pipe(gulp.dest('./dist/fonts')),
-  gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*')
-  .pipe(gulp.dest('./dist/fonts'));
+//Gulp clean up - mustn't happen asynchronously, so I'm not returning a stream
+gulp.task('clean', ['jshint'], function(){
+  return del(['dist']);
 });
 
-//Configuring the Watch Task
-gulp.task('watch', ['browser-sync'], function(){
-  //watch css and js files
-  gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['usemin', reload]);
+//Gulp usemin task - first method as described in the docs - weirdly throws an exception
+// gulp.task('usemin', function(){
+//   return gulp.src('app/*.html')
+//   .pipe(usemin({
+//     css: [cleancss(), rev()],
+//     js: [uglify(), rev()],
+//     html: [htmlmin({
+//       collapseWhitespace: true
+//     })],
+//     inlinejs: [uglify()],
+//     inlinecss: [cleancss()]
+//   }))
+//   .pipe(gulp.dest('dist/'));
+// });
 
-  //watch image files
-  gulp.watch('app/images/**/*', ['imagemin', reload]);
-
-});
-
-//browserSync task
-gulp.task('browser-sync', ['default'], function () {
-
-  browserSync.init(null, {
-    server: {
-      baseDir: "dist",
-      index: "menu.html"
-    }
+//Gulp usemin task - second method as described in the docs
+gulp.task('usemin', function(){
+  return gulp.src('app/*.html')
+  .pipe(usemin({
+    css: [function() {return cleancss({
+      keepSpecialComments: 0
+    });},rev, 'concat'],
+    //with uglify
+    // js: [gulpngannonate, function() {
+    //   return uglify({
+    //     mangle: true
+    //   });}, rev, 'concat'],
+    //no uglify - for debugging purposes
+    js: [gulpngannonate, rev, 'concat'],
+      html: [ function() {return htmlmin({
+        collapseWhitespace: true
+      });}]
+    }))
+    .pipe(gulp.dest('dist/'));
   });
-});
 
-//Default task
-gulp.task('default', ['clean'], function(){
-  gulp.start('usemin', 'imagemin', 'copyfonts');
-});
+  //Gulp copy fonts task - add new font-dependencies here
+  gulp.task('copyfonts', function(){
+    //bootstrap fonts
+    var bootstrap_stream = gulp.src('./bower_components/bootstrap/dist/fonts/**/*.{ttf,woff,eof,svg}*').pipe(gulp.dest('./dist/fonts'));
+    var fontawesome_stream = gulp.src('./bower_components/font-awesome/fonts/**/*.{ttf,woff,eof,svg}*').pipe(gulp.dest('./dist/fonts'));
+    return merge(bootstrap_stream, fontawesome_stream);
+  });
+
+  //browser-sync
+  gulp.task('browser-sync', ['default'], function(cb){
+    browserSync.init({
+      server: {
+        baseDir: "dist"
+        , index: "contactus.html"
+      }
+    });
+    cb();
+  });
+
+  //Gulp watch task - needs browser-sync pre-req because watch enforces reload
+  gulp.task('watch', ['browser-sync'], function(){
+    //watching javascript, css and html files and run usemin on change and reload browser to load new changes
+    gulp.watch('{app/scripts/**/*.js,app/styles/**/*.css,app/**/*.html}', ['update-distribution-usemin', reload]);
+    //watching image files and run imagemin on change and reload browser to load new changes
+    gulp.watch('app/images/**/*', ['imagemin', reload]);
+  });
+
+  //build-distribution task
+  gulp.task('build-distribution', ['usemin', 'imagemin', 'copyfonts']);
+
+  //the default task
+  gulp.task('default', function(cb){
+    runsequence('clean','build-distribution',cb);
+  });
+
+  //update-distribution-usemin
+  gulp.task('update-distribution-usemin', ['jshint'], function(cb){
+    runsequence('usemin', cb);
+  });
