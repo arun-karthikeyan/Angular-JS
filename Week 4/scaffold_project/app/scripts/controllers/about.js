d5e498ac5a1305d9ebe6b3901a3f02bb55c38579
mecodesta <the3drevolution@gmail.com>
2016-03-10 15:58:38 -0700

    yo and yeoman

---
+++ Week 4/scaffold_project/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scaffoldProjectApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the scaffoldProjectApp
+ */
+angular.module('scaffoldProjectApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ Week 4/scaffold_project/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name scaffoldProjectApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the scaffoldProjectApp
+ */
+angular.module('scaffoldProjectApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
